fc24aa1e2b0b6857a89a8ec0fe18d1c0513a539d
ahmed nabbaa <ahmed.nabaa@outlook.com>
2021-02-16 16:11:44 +0100

    second

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-const validemail =(str)=>{
 
-let at;
-let dot;
-for (i=0; i <str.length; i++){
-if (str[i] === "@")
-{
-    at = i;
-}
-
-if (str[i] === "."){
-    dot = i;
-}}
-
-if (at > 0 && dot >  at){
-    return   `${str} is valid`;
-}
-else {
-      return  `please enter valid ${str}`;
-}
-}
-
-console.log(validemail("ahmed@gmail.com"));
 
 const validemail2 =(str)=>{
     
